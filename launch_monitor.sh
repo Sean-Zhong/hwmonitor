a5ad357dc6d15c29237f0c88937609fa5512ed55
Sean Zhong <sean.zhong98@gmail.se>
2025-07-23 18:33:37 +0200

    Add start script

---
+++ launch_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# A 5-second delay can sometimes help ensure the desktop is fully ready
+sleep 5
+
+# Run the docker container
+docker run --rm -it \
+  --device=/dev/dri:/dev/dri \
+  -v /sys:/sys:ro \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  hwmonitor
